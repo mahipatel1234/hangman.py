a0de38c3d448ee1cfa1216afc754c139870af6ed
mahipatel1234 <mahipatel@usf.edu>
2024-07-18 23:49:48 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,9 +1,11 @@
-### This code displays a Python version of the classic-word guessing game, Hangman. The object of the game is to guess letters until the secret word has been correctly guessed. The player has 6 attempts before the full body of the man is hung.
+### This code displays a Python version of the classic-word guessing game, Hangman. 
+### The object of the game is to guess letters until the secret word has been correctly guessed. 
+### The player has 6 attempts before the full body of the man is hung, and the game is over.
 
 ### Imports random module, which includes random number generation-related functions.
 import random
 
-### Contains keyboard art strings, representing different stages of the Hangman.
+### Contains art strings that represent different stages of the Hangman.
 hangman1 = [
 """
 +---+
@@ -44,14 +46,14 @@ hangman1 = [
 ### Creates a list of words for the user to guess from. To simplify the game, I chose to use the colors of the rainbow. 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-### Tells the program to select a random word from the (colors) list, and is then converted to lowercase letters.
+### Tells the program to select a random word from the (colors) list, and convert them to lowercase letters.
 word = random.choice(colors).lower()
 
 ### Stores correctly and incorrectly guessed letters.
 guessed_correctly = []
 guessed_incorrectly = []
 
-### Defines number of attempts allowed and subtracts 1 atempt after each attempt. 
+### Defines number of attempts allowed and subtracts 1 after each incorrect guess. 
 tries = 6
 hangman_count = -1
 
@@ -60,7 +62,8 @@ print("Welcome to Hangman!")
 print("Please guess letters one at a time to decipher the secret word. The category is: colors of the rainbow.")
 
 ### Controls the game flow based on the remaining attempts. 
-### If-else statements determine the correctness of the guessed letters.
+### If-else statements determine the correctness of the guessed letters, providing feedback
+### after correct and incorrect guesses.
 while tries > 0:
     output = ''
     for letter in word:
@@ -89,4 +92,4 @@ while tries > 0:
 if tries>0:
     print("Congratulations! You won!")
 else:
-    print("Sorry! You lost.")
+    print("Sorry! You lost. Game over.")
